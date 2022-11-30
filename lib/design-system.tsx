de9029763cef81b783a08b94ba53c92ef63c2eb3
Max Holman <max@holmn.com>
2022-11-30 20:31:50 +0800

    feat: add light mode contrast schemes feat: integration mode chore: code tidy

--- lib/design-system.tsx
+++ lib/design-system.tsx
@@ -3,14 +3,18 @@ import { Box } from './core.js';
 import { genericThemeClass, sansSerifFontStyle } from './design-system.css.js';
 import { resetClass } from './reset.css.js';
 import {
-  darkClass,
-  mediaPrefersColorSchemeClass,
-  lightClass,
+  defaultBgFgClass,
   ColorScheme,
-  defaultColorThemeClass,
-  darkMoreContrastClass,
-  darkLessContrastClass,
   ContrastScheme,
+  darkClass,
+  darkLessContrastClass,
+  darkMoreContrastClass,
+  defaultColorThemeClass,
+  lightClass,
+  lightLessContrastClass,
+  lightMoreContrastClass,
+  mediaPrefersColorSchemeClass,
+  mediaPrefersContrastSchemeClass,
 } from './schemes/color.css.js';
 
 export const DesignSystem: FC<
@@ -18,39 +22,57 @@ export const DesignSystem: FC<
     contrastScheme?: ContrastScheme;
     colorScheme?: ColorScheme;
     className?: string;
+    integrationMode?: boolean;
   }>
-> = ({ children, className, colorScheme, contrastScheme }) => (
-  <Box
-    component="div"
-    className={[
-      resetClass,
-      sansSerifFontStyle,
-      genericThemeClass,
+> = ({ children, className, colorScheme, contrastScheme, integrationMode }) => {
+  // for readability and minification
+  const autoColorScheme = colorScheme === 'auto';
+  const darkColorScheme = colorScheme === 'dark';
+  const lightColorScheme = colorScheme === 'light';
+  const moreContrast = contrastScheme === 'more';
+  const lessContrast = contrastScheme === 'less';
 
-      defaultColorThemeClass,
+  return (
+    <Box
+      component="div"
+      className={[
+        resetClass,
+        sansSerifFontStyle,
+        genericThemeClass,
 
-      // mediaPrefersContrastSchemeClass,
-      // contrastScheme === 'more' && moreContrastClass,
-      // contrastScheme === 'less' && lessContrastClass,
+        // color theme
+        defaultColorThemeClass,
 
-      (!colorScheme || colorScheme === 'auto') && mediaPrefersColorSchemeClass,
+        // don't set fg/bg if we're integrating with existing frameworks
+        !integrationMode && defaultBgFgClass,
 
-      colorScheme === 'dark' && darkClass,
-      colorScheme === 'dark' &&
-        contrastScheme === 'more' &&
-        darkMoreContrastClass,
-      colorScheme === 'dark' &&
-        contrastScheme === 'less' &&
-        darkLessContrastClass,
+        // auto color
+        (!colorScheme || autoColorScheme) && mediaPrefersColorSchemeClass,
 
-      colorScheme === 'light' && lightClass,
+        // auto contrast
+        (!contrastScheme || autoColorScheme) && mediaPrefersContrastSchemeClass,
 
-      className,
-    ]}
-  >
-    {children}
-  </Box>
-);
+        // forced dark
+        darkColorScheme && darkClass,
+
+        // dark + forced contrast
+        darkColorScheme && moreContrast && darkMoreContrastClass,
+        darkColorScheme && lessContrast && darkLessContrastClass,
+
+        // forced light
+        lightColorScheme && lightClass,
+
+        // forced light + forced contrast
+        lightColorScheme && moreContrast && lightMoreContrastClass,
+        lightColorScheme && lessContrast && lightLessContrastClass,
+
+        className,
+      ]}
+    >
+      {children}
+    </Box>
+  );
+};
 
 export const Reset: FC<PropsWithChildren> = ({ children }) => (
   <Box component="div" className={resetClass}>
