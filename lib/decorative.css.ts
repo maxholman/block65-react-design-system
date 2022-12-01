b5851016f0739745ff0c980a697a198acefe6eba
Max Holman <max@holmn.com>
2022-12-01 19:33:17 +0800

    feat: tweak default divider look

--- lib/decorative.css.ts
+++ lib/decorative.css.ts
@@ -1,9 +1,9 @@
 import { style } from '@vanilla-extract/css';
 import { genericVars } from './design-system.css.js';
-import { colorThemeVars } from './schemes/color.css.js';
+import { colorThemeVars, contrastSchemeVars } from './schemes/color.css.js';
 import { hsl } from './utils.js';
 
 export const dividerStyle = style({
-  height: genericVars.border.weight.normal,
-  backgroundColor: hsl(colorThemeVars.accent.h, colorThemeVars.accent.s, '90%'),
+  height: genericVars.border.weight.thin,
+  backgroundColor: hsl(colorThemeVars.accent.h, 0, contrastSchemeVars.ink.l),
 });
