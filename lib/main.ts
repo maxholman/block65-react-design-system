e20b6dcf2604f24319b5af67f4898f0ee41dc1bb
Max Holman <max@holmn.com>
2022-12-05 13:50:54 +0800

    feat: add list exports

--- lib/main.ts
+++ lib/main.ts
@@ -5,6 +5,7 @@ export * from './design-system.js';
 export * from './forms.js';
 export * from './layout.js';
 export * from './grid.js';
+export * from './list.js';
 export * from './links.js';
 export * from './panel.js';
 export * from './typography.js';
