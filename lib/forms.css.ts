fd5e14f906e5af12a78b6afe0a8925756a940f20
Max Holman <max@holmn.com>
2022-12-05 14:15:12 +0800

    fix: radio/checkbox alignment improvements fix: use smaller input borders

--- lib/forms.css.ts
+++ lib/forms.css.ts
@@ -7,7 +7,7 @@ export const formInput = style({
   padding: genericVars.space.small,
   borderColor: hsl(0, 0, contrastSchemeVars.fg2.l),
   borderStyle: 'solid',
-  borderWidth: genericVars.border.weight.normal,
+  borderWidth: genericVars.border.weight.hairline,
   borderRadius: genericVars.radius.medium,
   fontSize: genericVars.text.size.normal,
   selectors: {
@@ -30,9 +30,17 @@ export const formInputCheckRadioBase = style([
   {
     padding: 'revert',
     cursor: 'pointer',
-    height: genericVars.text.size.large,
+    fontSize: '1em',
+    height: genericVars.text.size.medium,
     aspectRatio: '1/1',
     color: hsl(colorThemeVars.accent.h, 0, contrastSchemeVars.fg.l),
+    // alignSelf: 'center',
+    lineHeight: 1.5,
+    // WARN: this is a bit hacky to get perfect alignment
+    // and still support multiple lines for radio/check labels
+    // I'm unsure how to deal with it correctly as it is linked to line-height
+    // which doesnt apply to this input
+    marginTop: '0.1em',
     selectors: {
       '&:focus-visible,&.focus-visible': {
         outline: 'max(2px, 0.15em) solid currentColor',
@@ -48,10 +56,10 @@ export const formInputCheckRadioBase = style([
       '&::before': {
         content: '""',
         fontSize: genericVars.text.size.normal,
-        height: '0.7em', // careful with this as it causes the text to jump
+        height: '0.55em', // careful with this as it causes the text to jump
         aspectRatio: '1/1',
-        // transform: 'scale(0)',
-        // transition: '60ms transform ease-in-out',
+        transform: 'scale(0)',
+        transition: '100ms transform ease-in-out',
         boxShadow: `inset 1em 1em ${hsl(
           colorThemeVars.accent.h,
           colorThemeVars.accent.s,
@@ -74,12 +82,18 @@ export const formInputCheckRadioBase = style([
 ]);
 
 export const formInputCheckRadioWrapper = style({
+  display: 'grid',
   gridTemplateColumns: 'auto 1fr',
 });
 
 export const formInputCheckRadioLabel = style({
   alignSelf: 'center',
   gridColumn: 2,
+});
+
+export const formInputCheckRadioMessage = style({
+  gridColumn: 2,
+  gridRow: 2,
 });
 
 export const formInputCheckboxInput = style([
