baedd0bf2adb03a44e99615ef1512ef1f1810c72
Max Holman <max@holmn.com>
2022-12-23 23:31:09 +0800

    feat: allow props to be passed to info icon

--- lib/icons.tsx
+++ lib/icons.tsx
@@ -1,6 +1,6 @@
-import type { FC } from 'react';
+import type { FC, SVGAttributes } from 'react';
 
-export const Info: FC = () => (
+export const Info: FC<SVGAttributes<SVGElement>> = (props) => (
   <svg
     stroke="currentColor"
     fill="currentColor"
@@ -9,6 +9,7 @@ export const Info: FC = () => (
     height="1em"
     width="1em"
     xmlns="http://www.w3.org/2000/svg"
+    {...props}
   >
     <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path>
     <path d="M11 11h2v6h-2zm0-4h2v2h-2z"></path>
