7ec7631a80bbf84709a39ea3cd4300e4cc1afcc4
Max Holman <max@holmn.com>
2022-12-25 15:40:48 +0800

    chore: reorder exports for readability

--- lib/main.ts
+++ lib/main.ts
@@ -1,18 +1,20 @@
+export * from './design-system.js';
+
+export * from './core.js';
+export * from './layout.js';
+export * from './grid.js';
+
 export * from './badges.js';
 export * from './buttons.js';
 export * from './callout.js';
-export * from './core.js';
 export * from './decorative.js';
-export * from './design-system.js';
 export * from './forms.js';
-export * from './grid.js';
-export * from './layout.js';
 export * from './list.js';
 export * from './links.js';
 export * from './loaders.js';
 export * from './panel.js';
 export * from './typography.js';
 
-export { type ColorScheme, type ContrastScheme } from './schemes/color.css.js';
-export { type Tone } from './tone.css.js';
-export { type BadgeVariant } from './badges.css.js';
+export type { ColorScheme, ContrastScheme } from './schemes/color.css.js';
+export type { Tone } from './tone.css.js';
+export type { BadgeVariant } from './badges.css.js';
