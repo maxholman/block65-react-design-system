fb91f7cd53730fbc7fcb9cc2af69bfd3dad1e0be
Max Holman <max@holmn.com>
2023-02-18 16:41:16 +0800

    chore: lint + docs

--- lib/links.tsx
+++ lib/links.tsx
@@ -1,5 +1,5 @@
 import type { FC, PropsWithChildren } from 'react';
-import { Box, type BoxBasedComponentProps } from './core.js';
+import { Box, BoxBasedComponentProps } from './core.js';
 import { linkStyleVariant, type LinkWeight } from './links.css.js';
 import type { Merge } from './types.js';
 
@@ -22,6 +22,7 @@ export function differentOriginLinkProps(href: string) {
   return null;
 }
 
+// `TextLink` is expect to be wrapped in a `Text` component
 export const TextLink: FC<TextLinkProps> = ({
   component = 'a',
   children,
