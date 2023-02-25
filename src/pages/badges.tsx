f726abc5e49c36ed271f5d96eb4c7ab2318f566b
Max Holman <max@holmn.com>
2023-02-25 18:20:14 +0800

    chore: update examples

--- src/pages/badges.tsx
+++ src/pages/badges.tsx
@@ -7,8 +7,8 @@ import {
   Inline,
   Panel,
   Text,
-  type Tone,
   type BadgeVariant,
+  type Tone,
 } from '../../lib/main.js';
 
 const badgeVariantNames: BadgeVariant[] = [
@@ -28,69 +28,75 @@ const badgeToneNames: Tone[] = [
 ];
 
 export const BadgesPage: FC = () => (
-  <Panel variant="ghost" space="huge">
-    <Block>
-      <Heading level="3">Badges</Heading>
+  <>
+    <Inline alignItems="center">
+      <Heading level="4">Default Badge</Heading>
+      <Badge>Active</Badge>
+    </Inline>
+    <Panel variant="ghost" space="huge">
+      <Block>
+        <Heading level="3">Badges</Heading>
 
-      <Grid
-        cols={{
-          mobile: 1,
-          all: 2,
-        }}
-      >
-        {badgeVariantNames.map((variant) => (
-          <Panel variant="subtle" key={variant}>
-            <Heading level="4">{variant}</Heading>
+        <Grid
+          cols={{
+            mobile: 1,
+            all: 2,
+          }}
+        >
+          {badgeVariantNames.map((variant) => (
+            <Panel variant="subtle" key={variant}>
+              <Heading level="4">{variant}</Heading>
+              <Inline>
+                {badgeToneNames.map((tone) => (
+                  <Badge variant={variant} tone={tone} key={tone}>
+                    {tone}
+                  </Badge>
+                ))}
+              </Inline>
+            </Panel>
+          ))}
+        </Grid>
+
+        <Grid cols={3}>
+          <Panel variant="subtle">
             <Inline>
-              {badgeToneNames.map((tone) => (
-                <Badge variant={variant} tone={tone} key={tone}>
-                  {tone}
-                </Badge>
-              ))}
+              <Heading level="4">Default Badge</Heading>
+              <Badge>Active</Badge>
             </Inline>
+            <Text>And some text</Text>
           </Panel>
-        ))}
-      </Grid>
-
-      <Grid cols={3}>
-        <Panel variant="subtle">
-          <Inline>
-            <Heading level="4">Default Badge</Heading>
-            <Badge>Active</Badge>
-          </Inline>
-          <Text>And some text</Text>
-        </Panel>
-        <Panel variant="standard">
-          <Inline>
-            <Heading level="4" textOverflow="ellipsis">
-              DBS Visa 4352
-            </Heading>
-            <Badge tone="warn">Expires Soon</Badge>
-          </Inline>
-          <Text>And some text</Text>
-        </Panel>
-        <Panel variant="transparent">
-          <Inline>
-            <Heading level="4">Default Badge</Heading>
-            <Badge tone="promo">50% Off</Badge>
-          </Inline>
-          <Text>And some text</Text>
-        </Panel>
-        <Panel variant="ghost">
-          <Inline>
-            <Heading level="4">Trial Account</Heading>
-            <Badge tone="critical">Expired</Badge>
-          </Inline>
-          <Text>Upgrade now</Text>
-        </Panel>
-        <Panel variant="ghost">
-          <Inline>
-            <Heading level="4">Messages (4)</Heading>
-            <Badge tone="positive">New</Badge>
-          </Inline>
-          <Text>You've got mail!</Text>
-        </Panel>
-      </Grid>
-    </Block>
-  </Panel>
+          <Panel variant="standard">
+            <Inline>
+              <Heading level="4" textOverflow="ellipsis">
+                DBS Visa 4352
+              </Heading>
+              <Badge tone="warn">Expires Soon</Badge>
+            </Inline>
+            <Text>And some text</Text>
+          </Panel>
+          <Panel variant="transparent">
+            <Inline>
+              <Heading level="4">Default Badge</Heading>
+              <Badge tone="promo">50% Off</Badge>
+            </Inline>
+            <Text>And some text</Text>
+          </Panel>
+          <Panel variant="ghost">
+            <Inline>
+              <Heading level="4">Trial Account</Heading>
+              <Badge tone="critical">Expired</Badge>
+            </Inline>
+            <Text>Upgrade now</Text>
+          </Panel>
+          <Panel variant="ghost">
+            <Inline>
+              <Heading level="4">Messages (4)</Heading>
+              <Badge tone="positive">New</Badge>
+            </Inline>
+            <Text>You've got mail!</Text>
+          </Panel>
+        </Grid>
+      </Block>
+    </Panel>
+  </>
 );
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,22 +1,56 @@
 import type { FC } from 'react';
-import { Block, Button, Inline, Panel, Text } from '../../lib/main.js';
+import {
+  Badge,
+  Block,
+  Button,
+  Heading,
+  Inline,
+  Panel,
+  Text,
+} from '../../lib/main.js';
 
 export const LayoutPage: FC = () => (
-  <Panel variant="ghost">
-    <Block>Layouts are tight!</Block>
+  <Panel variant="ghost" space="huge">
     <Block>
       <Block>
-        <Text>Block</Text>
-        <Button>Button</Button>
-        <Button>Button</Button>
+        <Badge>Block</Badge>
+        <Button>Block</Button>
+        <Button>Block</Button>
+        <Button>Block</Button>
       </Block>
     </Block>
     <Block>
       <Inline>
         <Text>Inline</Text>
-        <Button>Button</Button>
-        <Button>Button</Button>
+        <Button>Inline</Button>
+        <Button>Inline</Button>
       </Inline>
     </Block>
+
+    <Heading>Inline justifySelf</Heading>
+    <Panel variant="ghost">
+      <Inline justifySelf="start">
+        <Badge>start</Badge>
+      </Inline>
+      <Inline justifySelf="center">
+        <Badge>center</Badge>
+      </Inline>
+      <Inline justifySelf="end">
+        <Badge>end</Badge>
+      </Inline>
+    </Panel>
+
+    <Heading>Block justifySelf</Heading>
+    <Panel variant="ghost" style={{ height: '20rem' }}>
+      <Block justifySelf="start">
+        <Badge>start</Badge>
+      </Block>
+      <Block justifySelf="center">
+        <Badge>center</Badge>
+      </Block>
+      <Block justifySelf="end">
+        <Badge>end</Badge>
+      </Block>
+    </Panel>
   </Panel>
 );
