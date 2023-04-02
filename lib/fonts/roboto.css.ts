212299fd19f0274942715c044d9dd1ff55afc7a3
Max Holman <max@holmn.com>
2023-04-02 12:48:01 +0800

    fix: reflect changes to numeric props
    WARN: the metrics are cut and pasted from Inter and are wrong!

--- lib/fonts/roboto.css.ts
+++ lib/fonts/roboto.css.ts
@@ -7,52 +7,83 @@ import { createTheme, style } from '@vanilla-extract/css';
 import { withUnit } from '../css-helpers.css.js';
 import { fontThemeVars } from '../typography.css.js';
 
-export const robotoMonoFontTheme = createTheme(fontThemeVars, {
-  huge: {
-    capHeight: withUnit(24),
+const robotoMonoFontTheme = createTheme(fontThemeVars, {
+  // aim for 2.5rem - 40px
+  '6': {
+    capHeight: withUnit(29.1),
     values: precomputeValues({
-      capHeight: 24,
-      leading: 42,
+      capHeight: 29.1,
+      leading: 29.1 * 1.5,
       fontMetrics,
     }),
   },
-  large: {
-    capHeight: withUnit(20),
+
+  // aim for 2rem - 32px
+  '5': {
+    capHeight: withUnit(23.3),
     values: precomputeValues({
-      capHeight: 20,
-      leading: 45,
+      capHeight: 23.3,
+      leading: 23.3 * 1.5,
       fontMetrics,
     }),
   },
-  medium: {
-    capHeight: withUnit(16),
+
+  // aim for 1.5rem - 24px
+  '4': {
+    capHeight: withUnit(17.5),
     values: precomputeValues({
-      capHeight: 16,
-      leading: 24,
+      capHeight: 17.5,
+      leading: 17.5 * 1.6,
       fontMetrics,
     }),
   },
-  normal: {
-    capHeight: withUnit(11.75),
+
+  '3': {
+    // aim for 1.25rem - 20px
+    capHeight: withUnit(14.6),
     values: precomputeValues({
-      capHeight: 11.75,
-      leading: 24,
+      capHeight: 14.6,
+      leading: 14.6 * 1.5,
       fontMetrics,
     }),
   },
-  small: {
-    capHeight: withUnit(8.7272),
+
+  '2': {
+    // aim for 1.1rem - 17.6px
+    capHeight: withUnit(12.8),
     values: precomputeValues({
-      capHeight: 8.7272,
-      // leading: 32,
+      capHeight: 12.8,
+      leading: 12.8 * 1.6,
       fontMetrics,
     }),
   },
-  tiny: {
+
+  '1': {
+    // aim for 1rem - 16px
+    capHeight: withUnit(11.636363),
+    values: precomputeValues({
+      capHeight: 11.636363,
+      leading: 11.636363 * 1.6,
+      fontMetrics,
+    }),
+  },
+
+  // aim for 0.75rem - 12px
+  '0': {
     capHeight: '8.727272',
     values: precomputeValues({
       capHeight: 8.727272,
-      // leading: 32,
+      leading: 8.727272 * 1.75,
+      fontMetrics,
+    }),
+  },
+
+  // aim for 0.5rem - 8px
+  '00': {
+    capHeight: '5.818181',
+    values: precomputeValues({
+      capHeight: 5.818181,
+      leading: 5.818181 * 1.75,
       fontMetrics,
     }),
   },
@@ -61,6 +92,6 @@ export const robotoMonoFontTheme = createTheme(fontThemeVars, {
 export const robotoMonoFontThemeClassName = style([
   robotoMonoFontTheme,
   {
-    fontFamily: "'Roboto Mono', monospace",
+    fontFamily: '"Roboto Mono", monospace',
   },
 ]);
