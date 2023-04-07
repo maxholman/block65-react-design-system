af8c3dbef1eaa770a893cb478ad7292c82e8468f
Max Holman <max@holmn.com>
2023-04-07 21:42:44 +0800

    fix: add forwardRef to Panel

--- lib/panel.tsx
+++ lib/panel.tsx
@@ -1,6 +1,11 @@
+import { forwardRef, type ForwardedRef } from 'react';
 import { Block, type BlockProps } from './layout.js';
 import { panelVariants, type PanelVariant } from './panel.css.js';
-import type { Merge, ReactHTMLAttributesHacked } from './types.js';
+import type {
+  Merge,
+  ReactHTMLAttributesHacked,
+  ReactHTMLElementsHacked,
+} from './types.js';
 
 export type PanelProps<T extends keyof ReactHTMLAttributesHacked = 'section'> =
   Merge<
@@ -10,16 +15,18 @@ export type PanelProps<T extends keyof ReactHTMLAttributesHacked = 'section'> =
     }
   >;
 
-export const Panel = <T extends keyof ReactHTMLAttributesHacked = 'section'>({
-  variant = 'standard',
-  className,
-  ...props
-}: PanelProps<T>) => (
-  <Block
-    component="section"
-    rounded="medium"
-    padding="5"
-    className={[className, panelVariants[variant]]}
-    {...props}
-  />
+export const Panel = forwardRef(
+  <T extends keyof ReactHTMLAttributesHacked = 'section'>(
+    { variant = 'standard', className, ...props }: PanelProps<T>,
+    ref: ForwardedRef<ReactHTMLElementsHacked[T]>,
+  ) => (
+    <Block
+      ref={ref}
+      component="section"
+      rounded="medium"
+      padding="5"
+      className={[className, panelVariants[variant]]}
+      {...props}
+    />
+  ),
 );
