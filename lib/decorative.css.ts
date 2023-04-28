f439ecb31b56ad836bd083338566ead7cb8e66a8
Max Holman <max@holmn.com>
2023-04-28 10:15:54 +0800

    feat: improve divider flexibility

--- lib/decorative.css.ts
+++ lib/decorative.css.ts
@@ -1,14 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { style } from '@vanilla-extract/css';
 import { genericVars } from './design-system.css.js';
-import { colorThemeVars, contrastSchemeVars } from './schemes/color.css.js';
-import { hsl } from './utils.js';
 
 export const dividerStyle = style({
   height: genericVars.border.width[1],
-  backgroundColor: hsl(
-    colorThemeVars.tones.accent.h,
-    0,
-    contrastSchemeVars.foreground2.l,
-  ),
 });
--- lib/decorative.tsx
+++ lib/decorative.tsx
@@ -2,7 +2,22 @@ import type { FC } from 'react';
 import { Box, type BoxBasedComponentProps } from './core.js';
 import { dividerStyle } from './decorative.css.js';
 
-export const Divider: FC<BoxBasedComponentProps<'hr'>> = ({
-  className,
-  ...props
-}) => <Box component="hr" className={[className, dividerStyle]} {...props} />;
+export type DividerProps = Pick<
+  BoxBasedComponentProps<'hr'>,
+  | 'className'
+  | 'margin'
+  | 'marginInline'
+  | 'marginBlock'
+  | 'padding'
+  | 'paddingInline'
+  | 'paddingBlock'
+>;
+
+export const Divider: FC<DividerProps> = ({ className, ...props }) => (
+  <Box
+    component="hr"
+    className={[className, dividerStyle]}
+    {...props}
+    background="2"
+  />
+);
