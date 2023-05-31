33fe4b593bd0150a762eecb4f5c5f0213c67f4f6
Max Holman <max@holmn.com>
2023-05-31 20:21:46 +0800

    core: allow dev deps in examples

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,7 +11,7 @@ module.exports = {
 
   overrides: [
     {
-      files: ['**/*.css.ts', 'src/pages/**/*.tsx'],
+      files: ['**/*.css.ts', 'src/**/*.tsx'],
       rules: {
         'import/no-extraneous-dependencies': [
           'error',
