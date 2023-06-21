3da3c7dc20c5fd35cb68f58d5c675045797b69db
Max Holman <max@holmn.com>
2023-06-21 15:37:44 +0800

    chore: use builtin styles/components

--- lib/avatar.css.ts
+++ lib/avatar.css.ts
@@ -1,30 +1,17 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { style, styleVariants, type StyleRule } from '@vanilla-extract/css';
+import { style } from '@vanilla-extract/css';
 import { calc } from '@vanilla-extract/css-utils';
-import { genericVars } from './design-system.css.js';
-import { contrastSchemeVars } from './schemes/color.css.js';
-import { toneH, toneS } from './tone.css.js';
+import type { Variant } from './layout.js';
+import { toneH } from './tone.css.js';
 import { currentCapHeight } from './typography.css.js';
-import { hsl } from './utils.js';
 
-export type AvatarVariant =
-  | 'normal'
-  | 'ghost'
-  | 'subtle'
-  | 'transparent'
-  | 'image';
+export type AvatarVariant = Exclude<Variant, 'none'> | 'image';
 
 export const avatarClassName = style({
-  vars: {
-    [toneS]: '60%',
-  },
   display: 'inline-grid',
   aspectRatio: '1/1',
   width: calc.multiply(currentCapHeight, 4),
   height: calc.multiply(currentCapHeight, 4),
   placeItems: 'center',
-  color: hsl(toneH, toneS, contrastSchemeVars.foreground4.l),
-  fontWeight: genericVars.text.weight.bold,
   textTransform: 'uppercase',
 });
 
@@ -43,39 +30,6 @@ export const identClasses = [
       vars: {
         [toneH]: `${Math.round(i * (360 / numberOfColours))}`,
       },
-      color: 'red',
     }),
   ),
 ] as const;
-
-const variantRules: Record<
-  AvatarVariant,
-  {
-    // outlineColor: string;
-  } & Pick<StyleRule, 'backgroundColor' | 'vars' | 'outlineColor' | 'color'>
-> = {
-  normal: {
-    backgroundColor: hsl(toneH, toneS, contrastSchemeVars.foreground3.l),
-    outlineColor: hsl(toneH, toneS, 100),
-  },
-  ghost: {
-    // backgroundColor: hsl(toneH, toneS, contrastSchemeVars.background1.l),
-    outlineColor: hsl(toneH, toneS, contrastSchemeVars.foreground2.l, 0.25),
-  },
-  subtle: {
-    backgroundColor: hsl(toneH, toneS, contrastSchemeVars.background2.l),
-    outlineColor: hsl(toneH, toneS, contrastSchemeVars.foreground4.l, 0.25),
-  },
-  transparent: {
-    backgroundColor: 'transparent',
-    outlineColor: 'transparent',
-  },
-  image: {
-    backgroundColor: hsl(0, 0, 90),
-    // outlineColor: 'transparent',
-  },
-};
-
-export const avatarVariants = styleVariants(variantRules, (variant) => [
-  variant,
-]);
--- lib/avatar.tsx
+++ lib/avatar.tsx
@@ -1,15 +1,13 @@
 import {
   avatarClassName,
   avatarImgClass,
-  avatarVariants,
   identClasses,
   type AvatarVariant,
 } from './avatar.css.js';
-import { Box } from './core.js';
-import { type InlineProps } from './layout.js';
+import { Box, type BoxProps } from './core.js';
+import { Flex, type InlineProps } from './layout.js';
 import type { Merge, ReactHTMLAttributesHacked } from './types.js';
-import { fontSizeVariantVars } from './typography.css.js';
-import { Text, type FontSize } from './typography.js';
+import { fontSizeVariantVars, type FontSize } from './typography.css.js';
 
 type CommonAvatarProps = {
   label: string;
@@ -51,24 +49,58 @@ function extractInitials(text: string): string {
   );
 }
 
+function getAvatarVariantProps(variant: AvatarVariant) {
+  const variantProps = {
+    vibrant: {
+      background: '10',
+      foreground: '0',
+    },
+    subtle: {
+      background: '5',
+      foreground: '12',
+    },
+    image: {
+      background: '0',
+    },
+    ghost: {
+      borderWidth: '4',
+      border: '6',
+      foreground: '6',
+      background: null,
+    },
+    solid: {
+      background: '6',
+      foreground: '0',
+    },
+    transparent: {
+      background: null,
+    },
+  } satisfies Record<AvatarVariant, BoxProps>;
+
+  return variantProps[variant];
+}
+
 export const Avatar = <T extends keyof ReactHTMLAttributesHacked>({
-  variant = 'normal',
   size = '1',
-  ident,
+  variant = 'solid',
   label,
+  ident = label,
   image,
   className,
   children,
 }: AvatarProps<T>) => (
-  <Box
+  <Flex
     rounded="maximum"
+    fontSize={size}
+    fontWeight="heavy"
+    tone={null} // we provide our own tone via the ident colour
     className={[
+      className,
       avatarClassName,
       fontSizeVariantVars[size],
-      avatarVariants[variant],
       getIdentClassName(ident),
-      className,
     ]}
+    {...getAvatarVariantProps(variant)}
   >
     {image ? (
       <Box
