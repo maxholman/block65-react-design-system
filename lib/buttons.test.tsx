b5988ef1148cd5484a3b045399f972ec61c9b62f
Max Holman <max@holmn.com>
2023-06-21 17:08:14 +0800

    chore: update snapshots

--- lib/buttons.test.tsx
+++ lib/buttons.test.tsx
@@ -16,10 +16,10 @@ describe('Button', () => {
     // ASSERT
     expect(screen.getByRole('button')).toMatchInlineSnapshot(`
       <button
-        class="typography_fontSizeVariantVars_1__16o63x1c buttons_buttonClassName__jqajj21 layout_flexWrapVariants_nowrap__17vpgba3 layout_alignItemsVariants_center__17vpgba8 layout_justifyContentVariants_center__17vpgbak core_viewport_all_space_2__1rbo53vo3 core_viewport_all_paddingBlock_4__1rbo53vg9 core_viewport_all_paddingInline_5__1rbo53vdn core_textAlignVariants_center__1rbo53v28 core_roundedVariants_medium__1rbo53v2 tone_toneVariants_accent__y2da5n2 core_borderVariants_6__1rbo53vsk core_borderBaseClass__1rbo53vs5 core_borderHoverVariants_6__1rbo53vt0 core_borderWidthVariants_2__1rbo53vs8 core_borderBaseClass__1rbo53vs5 core_backgroundVariants_7__1rbo53v11 core_bgStyle__1rbo53vt core_flexDirectionVariants_row__1rbo53vng"
+        class="buttons_buttonClassName__jqajj21 layout_flexWrapVariants_nowrap__17vpgba3 layout_alignItemsVariants_center__17vpgba8 layout_justifyContentVariants_center__17vpgbak core_viewport_all_space_2__1rbo53vo2 core_viewport_all_paddingBlock_5__1rbo53vg9 core_viewport_all_paddingInline_5__1rbo53vdm core_roundedVariants_medium__1rbo53v2 tone_toneVariants_accent__y2da5n2 core_borderVariants_6__1rbo53vsj core_borderBaseClass__1rbo53vs4 core_borderHoverVariants_7__1rbo53vt1 core_borderWidthVariants_2__1rbo53vs7 core_borderBaseClass__1rbo53vs4 core_backgroundVariants_6__1rbo53vz core_backgroundHoverVariants_7__1rbo53v1w core_flexDirectionVariants_row__1rbo53vnf typography_fontSizeVariantVars_1__16o63x1c capsize_capsizeStyle__1d0g9qk4 typography__16o63x1m"
       >
         <div
-          class="typography_textClass__16o63x17 typography_fontSizeVariantVars_1__16o63x1c capsize_capsizeStyle__1d0g9qk4 typography__16o63x1m core_textAlignVariants_center__1rbo53v28"
+          class="core_textAlignVariants_center__1rbo53v27 core_borderTransparentClass__1rbo53vst typography_fontSizeVariantVars_1__16o63x1c capsize_capsizeStyle__1d0g9qk4 typography__16o63x1m"
         >
           Click Me
         </div>
