3a2a9249293598da3e350732d5b3b319e28e9184
Max Holman <max@holmn.com>
2024-07-26 12:46:56 +0800

    chore: add commented alternative merge generic for later review

--- lib/types.ts
+++ lib/types.ts
@@ -1,4 +1,5 @@
 export type Merge<A, B> = Omit<A, keyof B> & B;
+// export type Merge<A, B> = { [K in Exclude<keyof A, keyof B>]: A[K] } & B;
 
 export type { ReactHTMLAttributesHacked } from './react-html-attributes.js';
 export type { ReactHTMLElementsHacked } from './react-html-elements.js';
